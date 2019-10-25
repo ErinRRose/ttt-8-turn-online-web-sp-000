c0cd913626317a187b4803976628980ba8fe4c2e
Erin  Rose <errose10@gmail.com>
2019-10-25 01:34:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,13 @@
 def turn(board)
   puts "Please enter 1-9:"
 end
+def valid_move?(board, index)
+    if index.between?(0, 8) && !position_taken?(board, index)
+      true
+    else
+    false
+    end
+end
 def position_taken?(board, index)
    if board[index] == "X" || board[index] == "O"
      true
